a750c463d74512ac3fd4955b66c749a92574e06e
Ivan Morales <ivan.morales@ndr.com>
2020-09-03 12:00:02 -0400

    Install google-map-react component and display initial map Set map container size to display map

--- incident-map/src/App.js
+++ incident-map/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import GoogleMapReact from 'import './App.css';
+import GoogleMapReact from 'google-map-react'
 
+const DEFAULT_CENTER = [34.0522, -118.2437]
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        defaultZoom={10}
+        defaultCenter={DEFAULT_CENTER}>
+      </GoogleMapReact>
     </div>
   );
 }
