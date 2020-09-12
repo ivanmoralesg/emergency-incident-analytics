9f31ceafa628383475e2121ddc094a537f648fa5
Ivan Morales <ivan.morales@ndr.com>
2020-09-12 10:00:16 -0400

    Move weather enrich logic to Spring web service backend RestController scaffold from start.spring.io

---
+++ enrich-service/src/main/java/com/firedept/incident/WeatherEnrichService.java
@@ -0,0 +1,88 @@
+package com.firedept.incident;
+
+import static java.util.Collections.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin
+@RestController
+public class WeatherEnrichService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${meteostat.api.key}")
+    private String meteostatApiKey;
+
+    @Value("${mapbox.access.token}")
+    private String mapBoxAccessToken;
+
+    @GetMapping("/mapboxAccessToken")
+    public Map<String, String> mapboxAccessToken() {
+        return singletonMap("accessToken", mapBoxAccessToken);
+    }
+
+    @GetMapping("/weather")
+    public Map<String, Object> weatherInfo(@RequestParam double latitude, @RequestParam double longitude,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssX") Date startTime) throws ParseException {
+
+        Map<String, Object> requestParams = new HashMap<>();
+        requestParams.put("lat", latitude);
+        requestParams.put("lon", longitude);
+        requestParams.put("startDate", String.format("%tF", startTime));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(singletonList(MediaType.ALL));
+        headers.set("x-api-key", meteostatApiKey);
+
+        ResponseEntity<Map> response = restTemplate.exchange(
+                "https://api.meteostat.net/v2/point/hourly?lat={lat}&lon={lon}&start={startDate}&end={startDate}",
+                HttpMethod.GET, new HttpEntity<>(headers), Map.class, requestParams);
+
+        int startHour = toHour(startTime);
+        List<Map<String, Object>> hourlyData = (List) response.getBody().get("data");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Optional<Map<String, Object>> weatherAtHour = hourlyData.stream().filter(w -> {
+            String timeStr = (String) w.get("time");
+            try {
+                int hour = toHour(dateFormat.parse(timeStr));
+                return hour == startHour;
+            } catch (ParseException e) {
+                System.err.println("Unable to parse Meteostat time: " + timeStr);
+                return false;
+            }
+        }).findFirst();
+
+        return weatherAtHour.orElse(new HashMap<>());
+
+    }
+
+    private int toHour(Date dateTime) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateTime);
+        
+        return calendar.get(Calendar.HOUR_OF_DAY);
+
+    }
+}
--- incident-map/src/components/IncidentMap.js
+++ incident-map/src/components/IncidentMap.js
@@ -4,20 +4,14 @@ const Leaflet = window.L;
 
 const IncidentMap = () => {
 
-    const DEFAULT_CENTER = [37.541885, -77.440624] // Richmond, VA
+    const DEFAULT_CENTER = [37.541885, -77.440624] // TODO calculate map center, default to Richmond, VA
     const DEFAULT_ZOOM = 11;
-
     const LEAFLET_ATTRIBUTION = 'Map data &copy; ' +
         '<a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
-    const MAPBOX_ACCESS_TOKEN = '';
-
-    const WEATHER_API_AUTH_HEADERS = {
-        headers: { 'x-api-key': '' }
-    };
-
+    const [mapboxAccessToken, setMapboxAccessToken] = useState('');
     const [leafletMap, setMap] = useState('');
     const [incidents, setIncidents] = useState([]);
 
@@ -26,33 +20,15 @@ const IncidentMap = () => {
         const baseResponse = await fetch(url);
         const baseData = await baseResponse.json();
         
-        const [eventId, latitude, longitude, startTime, city] = [
+        const [eventId, latitude, longitude, startTime] = [
             baseData.description.event_id,
             baseData.address.latitude,
             baseData.address.longitude,
-            baseData.description.event_opened,
-            baseData.address.city,
-            baseData.address.state
+            baseData.description.event_opened
         ];
 
-        const startDate = startTime.split('T')[0];
-        const hour = startTime.split('T')[1].split(':')[0];
-
-        const stationResponse = await fetch(`https://api.meteostat.net/v2/stations/search?query=${city}`, WEATHER_API_AUTH_HEADERS);
-        const weatherStations = await stationResponse.json();
-        let closestStationId = weatherStations.data[0].id;
-        let [minLatitudeDistance, minLongitudeDistance] = [ weatherStations.data[0].latitude, weatherStations.data[0].longitude ];    
-        weatherStations.data.forEach(s => {
-            if (s.latitude - latitude < minLatitudeDistance && s.longitude - longitude < minLongitudeDistance) {
-                closestStationId = s.id;
-            }
-        })
-
-        const weatherResponse = await fetch(`https://api.meteostat.net/v2/stations/hourly?station=${closestStationId}&start=${startDate}&end=${startDate}`, WEATHER_API_AUTH_HEADERS);
-        const weatherData = await weatherResponse.json();
-        const weatherAtIncidentHour = weatherData.data.find(hourlyData => {
-            return hour === hourlyData.time.split(' ')[1].split(':')[0]
-        });
+        const weatherResponse = await fetch(`http://localhost:8080/weather?latitude=${latitude}&longitude=${longitude}&startTime=${startTime}`);
+        const weatherAtIncidentHour = await weatherResponse.json();
 
         const incidentWeather = {
             eventId: eventId,
@@ -71,8 +47,11 @@ const IncidentMap = () => {
         updatedIncidents.push(incidentWeather);
         setIncidents(updatedIncidents);
 
-    } 
+    }
 
+    /**
+     * Load incident data, get weather from enrich service, and store in state
+     */
     useEffect(() => {
 
         const incidentFiles = [ 'F01705150050.json', 'F01705150090.json' ];
@@ -85,26 +64,45 @@ const IncidentMap = () => {
 
     }, [0]);
 
+    /**
+     * Fetch Mapbox access token
+     */
+    useEffect(() => {
+
+        const fetchMapboxAccessToken = async () => {
+            const response = await fetch('http://localhost:8080/mapboxAccessToken');
+            const json = await response.json()
+            setMapboxAccessToken(json.accessToken);
+        };
+
+        fetchMapboxAccessToken();
+
+    }, [mapboxAccessToken]);
+
+    /**
+     * Build initial map after Mapbox access token is available
+     */
     useEffect(() => {
         
-        if (leafletMap) {
+        if (mapboxAccessToken) {
             Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
                 attribution: LEAFLET_ATTRIBUTION,
                 maxZoom: 18,
                 id: 'mapbox/streets-v11',
                 tileSize: 512,
                 zoomOffset: -1,
-                accessToken: MAPBOX_ACCESS_TOKEN
+                accessToken: mapboxAccessToken
             }).addTo(leafletMap);
         } else {
             setMap(Leaflet.map('map').setView(DEFAULT_CENTER, DEFAULT_ZOOM));
         }    
 
-    }, [leafletMap]);
+    }, [mapboxAccessToken]);
 
     return (
         <div id="map" style={{ height: 800 }}>
             {
+                // Add incident markers and popups
                 incidents.map(i => {
                     const marker = Leaflet.marker([i.latitude, i.longitude]).addTo(leafletMap);
                     const popupText = `Temperature: ${i.weather.temperature}&deg;<br/>
